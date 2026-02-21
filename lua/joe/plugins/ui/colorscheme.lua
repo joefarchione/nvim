1257dc39fb11d4a4456b8e8fd5062f5eb6e42b9e
Joseph Farchione <jfarchione@gmail.com>
2026-02-21 16:41:19 -0500

    UI: Use ColorScheme autocmd for high-contrast overrides to ensure correct priority

--- lua/joe/plugins/ui/colorscheme.lua
+++ lua/joe/plugins/ui/colorscheme.lua
@@ -9,42 +9,49 @@ return {
       vim.g.nord_disable_background = false
       vim.g.nord_italic = false
 
+      -- --- Custom "Joe-Nord" High Contrast Overrides ---
+      local function apply_overrides()
+        local colors = {
+          nord7 = "#8fbcbb", -- Teal (Types)
+          nord8 = "#88c0d0", -- Cyan (Functions)
+          nord12 = "#d08770", -- Orange (Parameters/Arguments)
+          nord13 = "#ebcb8b", -- Yellow (Constants)
+          nord15 = "#b48ead", -- Purple (Keywords)
+        }
+
+        local hl = vim.api.nvim_set_hl
+
+        -- Parameters/Arguments (Orange + Italic)
+        hl(0, "@parameter", { fg = colors.nord12, italic = true, force = true })
+        hl(0, "@variable.parameter", { fg = colors.nord12, italic = true, force = true })
+
+        -- Types (Teal + Bold)
+        hl(0, "@type", { fg = colors.nord7, bold = true, force = true })
+        hl(0, "@type.builtin", { fg = colors.nord7, bold = true, force = true })
+        hl(0, "@type.definition", { fg = colors.nord7, bold = true, force = true })
+        hl(0, "@namespace", { fg = colors.nord7, force = true })
+
+        -- Functions & Constructors
+        hl(0, "@function", { fg = colors.nord8, force = true })
+        hl(0, "@constructor", { fg = colors.nord8, force = true })
+
+        -- Constants & Booleans
+        hl(0, "@constant", { fg = colors.nord13, bold = true, force = true })
+        hl(0, "@boolean", { fg = colors.nord13, bold = true, force = true })
+
+        -- Keywords (Purple)
+        hl(0, "@keyword", { fg = colors.nord15, force = true })
+        hl(0, "@keyword.function", { fg = colors.nord15, force = true })
+      end
+
+      -- Apply whenever the colorscheme changes
+      vim.api.nvim_create_autocmd("ColorScheme", {
+        pattern = "nord",
+        callback = apply_overrides,
+      })
+
       -- Load the base theme
       require("nord").set()
-
-      -- --- Custom "Joe-Nord" High Contrast Overrides ---
-      local colors = {
-        nord7 = "#8fbcbb", -- Teal (Types)
-        nord8 = "#88c0d0", -- Cyan (Functions)
-        nord9 = "#81a1c1", -- Blue (Variables)
-        nord12 = "#d08770", -- Orange (Parameters/Arguments)
-        nord13 = "#ebcb8b", -- Yellow (Constants)
-        nord15 = "#b48ead", -- Purple (Keywords)
-      }
-
-      local hl = vim.api.nvim_set_hl
-
-      -- Parameters/Arguments (Orange + Italic)
-      hl(0, "@parameter", { fg = colors.nord12, italic = true })
-      hl(0, "@variable.parameter", { fg = colors.nord12, italic = true })
-
-      -- Types (Teal + Bold)
-      hl(0, "@type", { fg = colors.nord7, bold = true })
-      hl(0, "@type.builtin", { fg = colors.nord7, bold = true })
-      hl(0, "@type.definition", { fg = colors.nord7, bold = true })
-      hl(0, "@namespace", { fg = colors.nord7 })
-
-      -- Functions & Constructors
-      hl(0, "@function", { fg = colors.nord8 })
-      hl(0, "@constructor", { fg = colors.nord8 })
-
-      -- Constants & Booleans
-      hl(0, "@constant", { fg = colors.nord13, bold = true })
-      hl(0, "@boolean", { fg = colors.nord13, bold = true })
-
-      -- Keywords (Purple)
-      hl(0, "@keyword", { fg = colors.nord15 })
-      hl(0, "@keyword.function", { fg = colors.nord15 })
     end,
   },
 }
